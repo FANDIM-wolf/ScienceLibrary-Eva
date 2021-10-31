5c6a3b0d5373133e9a69f22e261f6187c2888498
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2021-10-31 13:06:05 +0300

    Add files via upload

--- src/com/ScienceEva/MathPackage.java
+++ src/com/ScienceEva/MathPackage.java
@@ -85,4 +85,51 @@ public class MathPackage {
         }
     }
 
+
+    // functions to derivative ***
+    // Written by Chitra Nayal
+    private long derivativeTerm(String pTerm, long val)
+    {
+
+        // Get coefficient
+        String coeffStr = "";
+        int i;
+        for (i = 0; pTerm.charAt(i) != 'x' ; i++)
+        {
+            if(pTerm.charAt(i)==' ')
+                continue;
+            coeffStr += (pTerm.charAt(i));
+        }
+
+        long coeff = Long.parseLong(coeffStr);
+
+        // Get Power (Skip 2 characters for x and ^)
+        String powStr = "";
+        for (i = i + 2; i != pTerm.length() && pTerm.charAt(i) != ' '; i++)
+        {
+            powStr += pTerm.charAt(i);
+        }
+
+        long power=Long.parseLong(powStr);
+
+        // For ax^n, we return a(n-1)x^(n-1)
+        return coeff * power * (long)Math.pow(val, power - 1);
+    }
+    public long derivativeVal(String poly, int val)
+    {
+        long ans = 0;
+
+        int i = 0;
+        String[] stSplit = poly.split("\\+");
+        while(i<stSplit.length)
+        {
+            ans = (ans +derivativeTerm(stSplit[i], val));
+            i++;
+        }
+        return ans;
+    }
+    // ***
+
+
+
 }
