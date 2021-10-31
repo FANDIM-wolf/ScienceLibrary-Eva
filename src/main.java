1738d792e9a8b7002f04c063e6a91f87f9227320
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2021-10-31 13:13:09 +0300

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -5,8 +5,10 @@ import java.util.ArrayList;
 
 
 public class main {
+
     public static void main(String[] argc) throws IOException {
 
+        ArrayList<MathPackage> list = new ArrayList<MathPackage>();
         File file = new File("text.txt");
         BiologyPackage ObjectBiology = new BiologyPackage();
         MathPackage ObjectSpace = new MathPackage();
@@ -18,27 +20,29 @@ public class main {
 
         System.out.println(result);
 
-        ObjectSpace.SetPersonalCode();
+        //ObjectSpace.SetPersonalCode();
         //check for existing personal code
         ObjectSpace.ValidatePersonalCode(ObjectSpace);
         System.out.println(ObjectSpace.GetPersonalCode());
 
         ObjectSpace.AddInList(ObjectSpace);
+        ObjectSpace.AddInList(ObjectSpace);
+        list.add(ObjectSpace);
         ObjectSpace.ShowListOfObjects();
         Vector Vector_of_plane = new Vector();
         Vector_of_plane.x = 12;
         Vector_of_plane.y = 6;
         Vector_of_plane.z = 2;
         System.out.println("Magnitude Vector of Plane: "+Vector_of_plane.MagnitudeOfVector(Vector_of_plane));
+        FileAssistent assistent = new FileAssistent();
+        assistent.PutListDataInFile("package.txt" ,list );
 
-        FileWriter fw = new FileWriter("list.txt");
-        Writer output = new BufferedWriter(fw);
-        int sz = ObjectSpace.packages.size();
-        for(int i = 0 ; i < sz; i++){
-            output.write(ObjectSpace.packages.get(i).toString() + "\n");
 
-        }
-        output.close();
+        String str = "2x^2";
+        int val = 2;
+
+        System.out.println(ObjectSpace.derivativeVal(str, val));
+
 
     }
 
