13014ace4ede0d6ef9d3e7bd8da14ea6afb9840b
Mikhail Yolkin <63235817+FANDIM-wolf@users.noreply.github.com>
2021-11-02 11:58:15 +0300

    Add files via upload

--- src/main.java
+++ src/main.java
@@ -1,49 +1,54 @@
 import com.ScienceEva.*;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.*;
 import java.util.ArrayList;
+import javax.swing.*;
 
 
 public class main {
 
-    public static void main(String[] argc) throws IOException {
-
-        ArrayList<MathPackage> list = new ArrayList<MathPackage>();
-        File file = new File("text.txt");
-        BiologyPackage ObjectBiology = new BiologyPackage();
-        MathPackage ObjectSpace = new MathPackage();
-
-        ObjectSpace.setName("Roony");
-        ObjectBiology.setName("Miki");
-        int result = ObjectSpace.number_in_power(23);
-        ObjectBiology.GreetUserInConsole();
-
-        System.out.println(result);
-
-        //ObjectSpace.SetPersonalCode();
-        //check for existing personal code
-        ObjectSpace.ValidatePersonalCode(ObjectSpace);
-        System.out.println(ObjectSpace.GetPersonalCode());
-
-        ObjectSpace.AddInList(ObjectSpace);
-        ObjectSpace.AddInList(ObjectSpace);
-        list.add(ObjectSpace);
-        ObjectSpace.ShowListOfObjects();
-        Vector Vector_of_plane = new Vector();
-        Vector_of_plane.x = 12;
-        Vector_of_plane.y = 6;
-        Vector_of_plane.z = 2;
-        System.out.println("Magnitude Vector of Plane: "+Vector_of_plane.MagnitudeOfVector(Vector_of_plane));
-        FileAssistent assistent = new FileAssistent();
-        assistent.PutListDataInFile("package.txt" ,list );
+    static JPanel jPanel = new JPanel();
+    static  JFrame jFrame = getFrame();
+    public static void main(String[] argc) throws Exception{
 
 
-        String str = "2x^2";
-        int val = 2;
-
-        System.out.println(ObjectSpace.derivativeVal(str, val));
+        // get array of list
+        String [] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
 
 
+
+        jFrame.add(jPanel);
+        JButton jButton = new JButton( new MyAction());
+        jPanel.add(jButton);
+        jPanel.add(new JTextField(30));
+
+
+
+
+    }
+
+    static class MyAction extends AbstractAction{
+
+        @Override
+        public void actionPerformed(ActionEvent e){
+            jPanel.setBackground(Color.GREEN);
+        }
+    }
+
+    // function to display app
+    static  JFrame getFrame() {
+        JFrame jFrame = new JFrame() {};
+
+        jFrame.setVisible(true);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.setSize(500,300);
+        jFrame.setLocation(500,400);
+        jFrame.setBounds(100,100,1000,500);
+        jFrame.setTitle("Program with ScienceEva");
+        return  jFrame;
     }
 
 }
